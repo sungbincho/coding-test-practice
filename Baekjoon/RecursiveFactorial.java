46481ff3224de74b3890cfa46f6bb598534efcbf
sungbincho <sbcho834@gmail.com>
2021-11-07 23:14:28 +0900

    Baekjoon Recursive factorial 10872 success

---
+++ Baekjoon/RecursiveFactorial.java
@@ -0,0 +1,21 @@
+// 백준 재귀_10872_팩토리얼
+
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class RecursiveFactorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int num = sc.nextInt();
+        sc.close();
+        System.out.print(Factorial(num));
+    }
+    public static int Factorial(int num){
+        if(num <= 1)
+            return 1;
+        else
+            return (num * Factorial(num-1));
+    }
+}
