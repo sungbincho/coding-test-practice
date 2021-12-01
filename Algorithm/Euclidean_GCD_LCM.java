71b67fdf487d1d99d3c5c4dbc3d8ab3e023b11d2
sungbincho <sbcho834@gmail.com>
2021-12-01 15:29:59 +0900

    Euclidean_GCD_LCM

--- Algorithm/Euclidean_GCD_LCM.java
+++ Algorithm/Euclidean_GCD_LCM.java
@@ -5,13 +5,12 @@ public class Euclidean_GCD_LCM {
     // 재귀함수를 활용한 최대공약수 구하기
     public static int Recursive_GCD(int a, int b) {
         // 나머지가 0이라면 해당 수가 최종 최대 공약수이다.
-        if (a % b == 0) {
+        if (a % b == 0)
             return b;
-        }
+
         // 그렇지 않다면 제수를 다음 피제수로, 나머지를 다음 제수로 바꾸어 재귀를 진행한다.
-        else {
+        else
             return Recursive_GCD(b, a % b);
-        }
     }
 
     // 반복문을 활용한 최대공약수 구하기
